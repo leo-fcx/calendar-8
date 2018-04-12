919b2e689b835e2a31d203cbbe766e991b3a76e0
Leo <leo.fcx@gmail.com>
2018-04-12 09:34:24 -0400

    Adding styles to format months and days

--- src/calendar/month/index.js
+++ src/calendar/month/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import _ from 'lodash';
+import './Month.css'
+
+const months = [
+  'JAN',
+  'FEB',
+  'MAR',
+  'APR',
+  'MAY',
+  'JUN',
+  'JUL',
+  'AUG',
+  'SEP',
+  'OCT',
+  'NOV',
+  'DEC',
+];
 
 const Month = ({ month, year, days }) => (
   <div className="month">
-    <div className="header">{month} {year}</div>
+    <div className="header">{months[month]} {year}</div>
     <div className="body">
+      {
+        _.times(days[0].day, (value) => (
+          <div
+            key={`${year}${month}-empty-${value}`}
+            className="day"
+          >
+            &nbsp;
+          </div>
+        ))
+      }
       {
         days.map(item => (
           <div
             key={`${year}${month}${item.date}`}
-            className={`day ${item.isWeekend ? 'weekend' : ''} ${item.isHoliday ? 'holiday' : ''}`}
+            className={`day
+              ${item.inRange ? 'range' : ''}
+              ${item.isWeekend ? 'weekend' : ''}
+              ${item.isHoliday ? 'holiday' : ''}
+            `}
           >
             {item.date}
+          </div>
+        ))
+      }
+      {
+        _.times((6 - days[days.length - 1].day), (value) => (
+          <div
+            key={`${year}${month}-empty-${value}`}
+            className="day"
+          >
+            &nbsp;
           </div>
         ))
       }
