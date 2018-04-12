8e98d955f6517fde01bf3f00fa73bffafe513e28
Leo <leo.fcx@gmail.com>
2018-04-12 10:08:35 -0400

    Adding control to change Start date and country code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import Calendar from "./calendar";
 
+const getFormattedDate = (date) => {
+  const mm  = date.getMonth();
+  const dd  = date.getDate();
+
+  return [
+    date.getFullYear(),
+    (mm>9 ? '' : '0') + mm,
+    (dd>9 ? '' : '0') + dd
+  ].join('-');
+};
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -11,6 +22,13 @@ class App extends Component {
       totalDays: 30,
       countryCode: 'US'
     };
+    this.onStartDateChange = this.onStartDateChange.bind(this);
+  }
+
+  onStartDateChange (e) {
+    this.setState({
+      startDate: new Date(`${e.target.value} 00:00:00`)
+    })
   }
 
   render() {
@@ -22,11 +40,29 @@ class App extends Component {
         </header>
         <div className="controls">
           <label>
+            Start Date:
+            <input
+              type="date"
+              value={getFormattedDate(this.state.startDate)}
+              onChange={this.onStartDateChange}
+            />
+          </label>
+          <br />
+          <label>
             Number of days:
             <input
               type="number"
               value={this.state.totalDays}
               onChange={(e) => this.setState({totalDays: e.target.value})}
+            />
+          </label>
+          <br />
+          <label>
+            Country code:
+            <input
+              type="text"
+              value={this.state.countryCode}
+              onChange={(e) => this.setState({countryCode: e.target.value})}
             />
           </label>
         </div>
