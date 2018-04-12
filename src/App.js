8a58b980aa762a4e5c5990363186187225244348
Leo <leo.fcx@gmail.com>
2018-04-12 09:48:52 -0400

    Adding control to change Number of days

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import './App.css';
 import Calendar from "./calendar";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: new Date(),
+      totalDays: 30,
+      countryCode: 'US'
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,10 +20,20 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Calendar 8</h1>
         </header>
+        <div className="controls">
+          <label>
+            Number of days:
+            <input
+              type="number"
+              value={this.state.totalDays}
+              onChange={(e) => this.setState({totalDays: e.target.value})}
+            />
+          </label>
+        </div>
         <Calendar
-          startDate={new Date()}
-          totalDays={30}
-          countryCode={'US'}
+          startDate={this.state.startDate}
+          totalDays={this.state.totalDays}
+          countryCode={this.state.countryCode}
         />
       </div>
     );
